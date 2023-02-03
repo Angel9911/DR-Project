2c36f152c07ad795295cc66e722eab019650ef69
unknown <angel_dimitrov99@abv.bg>
2023-02-03 12:17:18 +0200

    Payment integration - in progress

--- ADExpress/src/main/java/com/example/demo/controllers/PaymentController.java
+++ ADExpress/src/main/java/com/example/demo/controllers/PaymentController.java
@@ -1,29 +1,46 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Customer;
 import com.example.demo.models.PaymentOrder;
 import com.example.demo.services.Impl.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.bind.ValidationException;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/paypal")
 public class PaymentController {
 
     private PaymentOrder paymentOrder;
     @Autowired
     private PaymentServiceImpl paymentService;
 
-    @RequestMapping(value="/make",method = RequestMethod.POST,produces = "application/json")
-    public ResponseEntity<PaymentOrder> makePayment(){
+    @RequestMapping(value="/make/payment",method = RequestMethod.POST,produces = "application/json")
+    public ResponseEntity<String> makePayment(@RequestBody PaymentOrder paymentOrder){
+        try{
+            String authorizePayment = paymentService.makePayment(paymentOrder);
+            System.out.println(authorizePayment);
+            return new ResponseEntity<>(authorizePayment, HttpStatus.OK);
+        }catch(Exception exception){
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
     }
-    @RequestMapping(value="/complete",method = RequestMethod.POST,produces = "application/json")
+    @RequestMapping(value="/complete/payment",method = RequestMethod.POST,produces = "application/json")
     public ResponseEntity<PaymentOrder> completePayment(){
+        PaymentOrder paymentOrder1 = new PaymentOrder();
+        return new ResponseEntity<>(paymentOrder1, HttpStatus.OK);
+
+    }
+    @RequestMapping(value="/cancel/payment",method = RequestMethod.POST,produces = "application/json")
+    public ResponseEntity<PaymentOrder> cancelPayment(){
+        PaymentOrder paymentOrder1 = new PaymentOrder();
+        return new ResponseEntity<>(paymentOrder1, HttpStatus.OK);
 
     }
 
--- ADExpress/src/main/java/com/example/demo/services/PaymentService.java
+++ ADExpress/src/main/java/com/example/demo/services/PaymentService.java
@@ -1,8 +1,21 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Customer;
+import com.example.demo.models.Packages;
 import com.example.demo.models.PaymentOrder;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.Transaction;
+
+import java.net.URI;
+import java.util.List;
 
 public interface PaymentService {
-    PaymentOrder makePayment();
+    String makePayment(PaymentOrder paymentOrder) throws Exception;
     PaymentOrder executePayment();
+    Payer getPayerInformation(Customer customer);
+    List<Transaction> getTransactionInfo(PaymentOrder paymentOrder);
+    RedirectUrls getRedirectsUrl();
+    String approvalLink(Payment approvedPayment);
 }
